ed516cf9b88bcb5076655f3c46309bbff88d4f68
yvetteveve <yvetteumubyeyi2@gmail.com>
2019-03-04 11:19:57 +0200

    i will edit in view.py for to add another new things in this file

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,30 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import Required
 
-
-# class ReviewForm(FlaskForm):
-
-#  title = StringField('Review title', validators=[Required()])
-
-#  review = TextAreaField('Pitch review')
- 
-
-#  submit = SubmitField('Submit')
-
-
-class UpdateProfile(FlaskForm):
-
-    submit = SubmitField('Submit')
-
-
-class CreatePitchs(FlaskForm):
-    pitch = TextAreaField('describe your business', validators=[Required()])
-    submit = SubmitField('Submit')
-
+class AddPostForm(FlaskForm):
+    title=StringField('Title',validators = [Required()])
+    content=TextAreaField('Content',validators = [Required()])
+    image=StringField('Image url',validators = [Required()])
+    submit=SubmitField('SUBMIT')
 
 class CommentForm(FlaskForm):
-    comment = TextAreaField('leave The The Comment', validators=[Required()])
+   
+   username = StringField('Enter your name',validators=[Required()])
+   comment = TextAreaField('pitch comment', validators=[Required()])
+   submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
 
-class registretion(FlaskForm):
+class SubscriptionForm(FlaskForm):
+    name=StringField('Name',validators =[Required()])
+    email=StringField('Email',validators =[Required()])
+    submit = SubmitField('Submit')
+
+class UpdatePostForm(FlaskForm):
+    title=StringField('Title',validators = [Required()])
+    content=TextAreaField('Content',validators = [Required()])
+    submit=SubmitField('SUBMIT')
--- app/main/views.py
+++ app/main/views.py
@@ -1,29 +1,123 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for, abort
 from . import main
-
-
-from flask import render_template, request, redirect, url_for, abort
-from ..models import User,Role,blog,Comment
-from .forms import UpdateProfile, Createblogs, CommentForm
-from .. import db
+from ..request import get_quote
+from .forms import CommentForm,UpdateProfile,AddPostForm,SubscriptionForm,UpdatePostForm
+from .. import db,photos
+from ..models import Quote,Post,User,Comment,Subscription
 from flask_login import login_required, current_user
-import markdown2
+from ..email import mail_message
 
+# import markdown2 
+# from flask_fontawesome import FontAwesome
 
-
-@main.route('/')
+@main.route('/', methods = ['GET', 'POST'])
 def index():
-    '''
+  '''
     View root page function that returns the index page and its data
     '''
-    title = 'blog'
-    blog = blog.query.all()
-    return render_template('index.html', title=title, blog=blog)
+  form=SubscriptionForm()
+  if form.validate_on_submit():
+        name = form.name.data
 
-@main.route('/user/<uname>/update', methods=['GET', 'POST'])
+        email= form.email.data
+        new_subscriber=Subscription(name=name,email=email)
+        db.session.add(new_subscriber)
+        db.session.commit()
+
+        mail_message("Thank you for subscribing","email/welcome_user",new_subscriber.email,user=new_subscriber)
+
+        return redirect(url_for('main.index'))
+  quote=get_quote()
+  posts=Post.get_posts()
+  title="Home| Welcome to MeBlog"
+
+  return render_template('index.html',title=title,quote=quote,posts=posts,subscription_form=form)
+
+@main.route('/post/<int:id>')
+def single_post(id):
+    post=Post.query.filter_by(id=id).first()
+    comments=Comment.get_comments(id=id)
+    return render_template('single_post.html',post=post,comments=comments)
+
+@main.route('/delete/comment/<int:id>', methods = ['GET', 'POST'])
+@login_required
+def delete_comment(id):
+    comment=Comment.query.filter_by(id=id).first()
+ 
+
+    if comment is not None:
+       comment.delete_comment()
+       return redirect(url_for('main.index'))
+
+@main.route('/delete/post/<int:id>', methods = ['GET', 'POST'])
+@login_required
+def delete_post(id):
+    post=Post.query.filter_by(id=id).first()
+ 
+
+    if post is not None:
+       post.delete_post(id)
+       return redirect(url_for('main.index'))
+
+ 
+
+@main.route('/post/new', methods = ['GET', 'POST'])
+@login_required
+def add_post():
+    form = AddPostForm()
+    
+    if form.validate_on_submit():
+        title = form.title.data
+
+        post= form.content.data
+        image=form.image.data
+
+        new_post = Post(content=post, title = title,image=image)
+        new_post.save_post()
+
+        subscribers=Subscription.query.all()
+        for subscriber in subscribers:
+           mail_message("New Post","email/send_email",subscriber.email,user=subscriber,post=new_post)
+
+        return redirect(url_for('main.index'))
+
+    
+
+    title = 'Add Post| Blog ...Blog'    
+    return render_template('post.html', title = title, post_form = form)
+
+
+@main.route('/new/comment/<int:id>', methods = ['GET','POST'])
+def add_comment(id):
+  post=Post.query.filter_by(id=id).first()
+  if post is None:
+    abort(404)
+
+  form=CommentForm()
+  if form.validate_on_submit():
+     name=form.username.data
+     comment=form.comment.data
+     new_comment=Comment(content=comment ,post=post,username=name)
+     db.session.add(new_comment)  
+     db.session.commit() 
+     
+     return redirect(url_for('main.index'))
+  return render_template('comment.html', comment_form=form)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
 def update_profile(uname):
-    user = User.query.filter_by(username=uname).first()
+    user = User.query.filter_by(username = uname).first()
     if user is None:
         abort(404)
 
@@ -35,40 +129,38 @@ def update_profile(uname):
         db.session.add(user)
         db.session.commit()
 
+        return redirect(url_for('.profile',uname=user.username))
 
-    return render_template('profile/update.html', form=form)
+    return render_template('profile/update.html',form =form)
 
-
-@main.route('/blog', methods=['GET', 'POST'])
-def create_blogs():
-    form = Createblogs()
-    if form.validate_on_submit():
-
-        blog = form.blog.data
-
-        new_blog = blog(blog=blog, user_id=current_user.id)
-
-        db.session.add(new_blog)
-        db.session.commit()
-
-        return redirect(url_for('main.index'))
-
-    return render_template('blog.html', form=form, user=current_user)
-
-
-@main.route('/blog/comment/<int:id>', methods=['GET', 'POST'])
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
 @login_required
-def create_comments(id):
-    form = CommentForm()
-   
-    if form.validate_on_submit():
-
-        comment = form.comment.data
-
-        new_comment = Comment(comment=comment, blog_id=id,user_id=current_user.id)
-        db.session.add(new_comment)
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
         db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
 
-    comment = Comment.query.filter_by(blog_id=id).all()
+@main.route('/edit/post/<int:id>',methods= ['GET','POST'])
+@login_required
+def update_post(id):
+   post=Post.query.filter_by(id=id).first()
+   if post is None:
+        abort(404)
 
-    return render_template('comment.html', comment=comment, form=form)
+   form=UpdatePostForm()
+#    form.title.data=post.title
+#    form.content.data=post.content
+#    post1=Post.query.filter_by(id=id).first()
+   if form.validate_on_submit():
+         post.title=form.title.data
+         post.content=form.content.data
+
+         db.session.add(post)
+         db.session.commit()
+
+         return redirect(url_for('main.index'))
+   return render_template('update_post.html',form=form)
