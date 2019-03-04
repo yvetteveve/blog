a5daf7140fd8aeec94fbe9d07e527674d9039631
yvetteveve <yvetteumubyeyi2@gmail.com>
2019-03-04 10:51:44 +0200

    i will edit in config.py file

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 
 class Config:
 
-    pitch_API_BASE_URL ='https://api.thepitchdb.org/3/pitch/{}?api_key={}'
-    pitch_API_KEY = os.environ.get('pitch_API_KEY')
+    QUOTE_API_BASE_URL = "http://quotes.stormconsultancy.co.uk/random.json"
+    QUOTE_API_KEY = os.environ.get('QUOTE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:123@localhost/pick'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:123@localhost/blog'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
 #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
@@ -23,10 +23,13 @@ class ProdConfig(Config):
     pass
 
 
+
+
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://wecode:123@localhost/pick'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:123localhost/blog'
+    DEBUG = True
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:123@localhost/pick'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:123localhost/blog'
     DEBUG = True
 
 
